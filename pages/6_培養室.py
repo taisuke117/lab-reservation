9cd5d9153685ba08041e9eec5b60473d89dd2c13
taisuke117 <76957542+taisuke117@users.noreply.github.com>
2026-03-30 20:49:57 +0900

    Update 6_培養室.py

--- pages/6_培養室.py
+++ pages/6_培養室.py
@@ -7,8 +7,8 @@ st.set_page_config(page_title="培養室予約", layout="wide")
 st.title("🧫 培養室 予約")
 
 equipment_colors = {
-    "安全キャビネット": "#059349",
-    "クリーンベンチ": "#00B050",
+    "安全キャビネット": "#2F4F4F",
+    "クリーンベンチ": "#333333",
 }
 
 show_calendar_page("培養室", equipment_colors, "culture_calendar")
