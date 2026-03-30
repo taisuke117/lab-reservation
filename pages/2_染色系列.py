3a6c3ce30804323285d96bff6550d8960ce6e01c
taisuke117 <76957542+taisuke117@users.noreply.github.com>
2026-03-30 09:37:10 +0900

    Update 2_染色系列.py

--- pages/2_染色系列.py
+++ pages/2_染色系列.py
@@ -3,11 +3,13 @@ import sys
 sys.path.append("/mount/src/lab-reservation")
 from utils import show_calendar_page
 
-st.set_page_config(page_title="染色系列予約", layout="wide")
-st.title("🎨 染色系列 予約")
+st.set_page_config(page_title="実験台（実験室）予約", layout="wide")
+st.title("🎨 実験台（実験室） 予約")
 
 equipment_colors = {
-    "染色系列": "#D86ECC",
+    "実験台１": "#C00000",
+    "実験台２": "#78206E",
+    "実験台３": "#D86ECC",
 }
 
-show_calendar_page("染色系列", equipment_colors, "staining_calendar")
+show_calendar_page("実験台（実験室）", equipment_colors, "staining_calendar")
