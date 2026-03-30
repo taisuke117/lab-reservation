5b4b0bcbb47a9e95676495393b1be527971c7eb5
taisuke117 <76957542+taisuke117@users.noreply.github.com>
2026-03-30 20:49:20 +0900

    Update 4_バーチャル撮影装置.py

--- pages/4_バーチャル撮影装置.py
+++ pages/4_バーチャル撮影装置.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="バーチャル撮影装置予約", layout="wide"
 st.title("🎥 バーチャル撮影装置 予約")
 
 equipment_colors = {
-    "バーチャル撮影装置": "#747474",
+    "バーチャル撮影装置": "#8B008B",
 }
 
 show_calendar_page("バーチャル撮影装置", equipment_colors, "virtual_calendar")
