20027d234211d8c8d3069a0aaee90ac0f83703a5
taisuke117 <76957542+taisuke117@users.noreply.github.com>
2026-05-20 10:57:05 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import streamlit as st
-from utils import add_noindex   # ← 追加
+from utils import add_noindex
 
 
 st.set_page_config(page_title="NDUP機器 予約システム", layout="wide")
-add_noindex()   # ← set_page_config の直後に追加
+add_noindex()
 
 st.title("🔬 NDUP機器 予約システム")
 st.markdown("---")
