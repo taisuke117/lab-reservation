a1798d69d84e10fd5cdd6e94950260857b66a33d
taisuke117 <76957542+taisuke117@users.noreply.github.com>
2026-05-20 11:10:02 +0900

    Update 6_培養室.py

--- pages/6_培養室.py
+++ pages/6_培養室.py
@@ -4,7 +4,7 @@ sys.path.append("/mount/src/lab-reservation")
 from utils import show_calendar_page
 from utils import add_noindex
 
-st.set_page_config(page_title="Cell culture", layout="wide")
+st.set_page_config(page_title="Cell Culture", layout="wide")
 add_noindex()
 st.title("🧫 培養室 予約")
 
