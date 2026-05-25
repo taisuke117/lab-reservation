c8b245512501cf3e348d16c224eedbe1b4d3dad4
taisuke117 <76957542+taisuke117@users.noreply.github.com>
2026-05-25 09:15:42 +0900

    Update 2_実験台（実験室）.py

--- pages/2_実験台（実験室）.py
+++ pages/2_実験台（実験室）.py
@@ -3,9 +3,10 @@ import sys
 sys.path.append("/mount/src/lab-reservation")
 from utils import show_calendar_page
 from utils import add_noindex
-
+from utils import check_password
 st.set_page_config(page_title="Lab Bench", layout="wide")
 add_noindex()
+check_password()
 st.title("🎨 実験台（実験室） 予約")
 
 equipment_colors = {
