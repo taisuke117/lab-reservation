8aa1ce6b62bc01fc113b250b6e6bf3ba742918b2
taisuke117 <76957542+taisuke117@users.noreply.github.com>
2026-05-25 09:15:25 +0900

    Update 1_ミクロトーム.py

--- pages/1_ミクロトーム.py
+++ pages/1_ミクロトーム.py
@@ -3,10 +3,13 @@ import sys
 sys.path.append("/mount/src/lab-reservation")
 from utils import show_calendar_page
 from utils import add_noindex
+from utils import check_password
 
 st.set_page_config(page_title="Microtome", layout="wide") 
 add_noindex()
+check_password() 
 st.title("🔪 ミクロトーム 予約")
+
 
 equipment_colors = {
     "回転式ミクロトーム（A）": "#8B0000",
