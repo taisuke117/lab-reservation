16cfdf8b8bda355642904419bb01d6943583c7f5
taisuke117 <76957542+taisuke117@users.noreply.github.com>
2026-05-25 09:15:58 +0900

    Update 3_包埋ロータリー・包埋センター.py

--- pages/3_包埋ロータリー・包埋センター.py
+++ pages/3_包埋ロータリー・包埋センター.py
@@ -3,9 +3,11 @@ import sys
 sys.path.append("/mount/src/lab-reservation")
 from utils import show_calendar_page
 from utils import add_noindex
+from utils import check_password
 
 st.set_page_config(page_title="Embedding", layout="wide")
 add_noindex()
+check_password() 
 st.title("📦 包埋ロータリー・包埋センター 予約")
 
 equipment_colors = {
